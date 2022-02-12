a4dee2eb1c350bf3797ca66fe4e79ec6d8bf9157
cosimawest <44038882+cosimawest@users.noreply.github.com>
2022-02-12 16:35:41 -0500

    Update 3Sum.cpp

--- Apple/3Sum.cpp
+++ Apple/3Sum.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
 
+        if(nums.size() < 3) return {};
+        if(nums[0] > 0) return {};
+
+        vector<vector<int>> result;
+        unordered_map<int, int> twoSum;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            twoSum[nums[i]] = i;
+        }
+        
+        
+        for(int i = 0; i < nums.size() - 2; i++) {
+            if(nums[i] > 0) return result;
+            
+            for(int j = i + 1; j < nums.size(); j++) {
+                int expected = -1 * (nums[i] + nums[j]);
+                
+                if(twoSum.count(expected) && twoSum.find(expected)->second > j)
+                    result.push_back({nums[i], nums[j], expected});
+                
+                j = twoSum.find(nums[j])->second;
+            }
+            i = twoSum.find(nums[i])->second;
+        }
+        
+        return result;
+    }
+};
