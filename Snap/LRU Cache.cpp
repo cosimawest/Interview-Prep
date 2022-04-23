04db6b3237dfabdb7028d8b965ec86291651174f
cosimawest <44038882+cosimawest@users.noreply.github.com>
2022-04-23 17:37:54 -0400

    Update LRU Cache.cpp

--- Snap/LRU Cache.cpp
+++ Snap/LRU Cache.cpp
@@ -1,212 +1,41 @@
 class LRUCache {
 public:
-    unordered_map<int, list<pair<int, int>>::iterator> cache; 
-    list<pair<int, int>> Lcache;
-    int cap;
+    unordered_map<int, list<pair<int, int>>::iterator> cache; // keeps track iterator pos/val of li item
+    list<pair<int, int>> li; // keeps track of LRU (through order)
+    int cap; // obvi
     
-
-    LRUCache(int capacity) { cap = capacity; }
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
     
-    int get(int key) {
-        if(cache.find(key) == cache.end()) return -1;
+    int get(int key) { 
+        if(cache.find(key) == cache.end()) // key doesn't exist
+            return -1;
         
-        Lcache.splice(Lcache.begin(), Lcache, cache[key]); // update list
+        // move item to front of list
+        li.splice(li.begin(), li, cache[key]);
+
         return cache[key]->second;
     }
     
-    void put(int key, int value) {        
-        if (get(key) != -1) { // if exists, replace value THATS IT (get function updates list)
+    void put(int key, int value) {
+        if(get(key) != -1) { // key exists
+            // item already moved to front of li in get()
             cache[key]->second = value;
             return;
         }
-
-        if (cache.size() == cap) { // if !exists and cap, delete from map -> delete from list
-            int delKey = Lcache.back().first;
-            Lcache.pop_back();
-            cache.erase(delKey);
-        }
-        // if !exists, 
-        Lcache.emplace_front(key, value);
-        cache[key] = Lcache.begin();
-    }
-};
-
-// LIST.emplace_front => put item in front of list
-// LIST.splice(where to put position, list2, key to find); => move item to position in list
-// LIST.pop_back(); => remove last item in list
-// MAP.erase(); => erase item in map
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */class LRUCache {
-public:
-    unordered_map<int, list<pair<int, int>>::iterator> cache; 
-    list<pair<int, int>> Lcache;
-    int cap;
-    
-
-    LRUCache(int capacity) { cap = capacity; }
-    
-    int get(int key) {
-        if(cache.find(key) == cache.end()) return -1;
         
-        Lcache.splice(Lcache.begin(), Lcache, cache[key]); // update list
-        return cache[key]->second;
-    }
-    
-    void put(int key, int value) {        
-        if (get(key) != -1) { // if exists, replace value THATS IT (get function updates list)
-            cache[key]->second = value;
-            return;
+        if(cache.size() == cap) { // key doesn't exist
+            // removed LRU in li and cache
+            cache.erase(li.back().first);
+            li.pop_back();
         }
-
-        if (cache.size() == cap) { // if !exists and cap, delete from map -> delete from list
-            int delKey = Lcache.back().first;
-            Lcache.pop_back();
-            cache.erase(delKey);
-        }
-        // if !exists, 
-        Lcache.emplace_front(key, value);
-        cache[key] = Lcache.begin();
-    }
-};
-
-// LIST.emplace_front => put item in front of list
-// LIST.splice(where to put position, list2, key to find); => move item to position in list
-// LIST.pop_back(); => remove last item in list
-// MAP.erase(); => erase item in map
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */class LRUCache {
-public:
-    unordered_map<int, list<pair<int, int>>::iterator> cache; 
-    list<pair<int, int>> Lcache;
-    int cap;
-    
-
-    LRUCache(int capacity) { cap = capacity; }
-    
-    int get(int key) {
-        if(cache.find(key) == cache.end()) return -1;
         
-        Lcache.splice(Lcache.begin(), Lcache, cache[key]); // update list
-        return cache[key]->second;
-    }
-    
-    void put(int key, int value) {        
-        if (get(key) != -1) { // if exists, replace value THATS IT (get function updates list)
-            cache[key]->second = value;
-            return;
-        }
-
-        if (cache.size() == cap) { // if !exists and cap, delete from map -> delete from list
-            int delKey = Lcache.back().first;
-            Lcache.pop_back();
-            cache.erase(delKey);
-        }
-        // if !exists, 
-        Lcache.emplace_front(key, value);
-        cache[key] = Lcache.begin();
+        // add new item to cache and li
+        li.emplace_front(key, value);
+        cache[key] = li.begin();
     }
 };
-
-// LIST.emplace_front => put item in front of list
-// LIST.splice(where to put position, list2, key to find); => move item to position in list
-// LIST.pop_back(); => remove last item in list
-// MAP.erase(); => erase item in map
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */class LRUCache {
-public:
-    unordered_map<int, list<pair<int, int>>::iterator> cache; 
-    list<pair<int, int>> Lcache;
-    int cap;
-    
-
-    LRUCache(int capacity) { cap = capacity; }
-    
-    int get(int key) {
-        if(cache.find(key) == cache.end()) return -1;
-        
-        Lcache.splice(Lcache.begin(), Lcache, cache[key]); // update list
-        return cache[key]->second;
-    }
-    
-    void put(int key, int value) {        
-        if (get(key) != -1) { // if exists, replace value THATS IT (get function updates list)
-            cache[key]->second = value;
-            return;
-        }
-
-        if (cache.size() == cap) { // if !exists and cap, delete from map -> delete from list
-            int delKey = Lcache.back().first;
-            Lcache.pop_back();
-            cache.erase(delKey);
-        }
-        // if !exists, 
-        Lcache.emplace_front(key, value);
-        cache[key] = Lcache.begin();
-    }
-};
-
-// LIST.emplace_front => put item in front of list
-// LIST.splice(where to put position, list2, key to find); => move item to position in list
-// LIST.pop_back(); => remove last item in list
-// MAP.erase(); => erase item in map
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */class LRUCache {
-public:
-    unordered_map<int, list<pair<int, int>>::iterator> cache; 
-    list<pair<int, int>> Lcache;
-    int cap;
-    
-
-    LRUCache(int capacity) { cap = capacity; }
-    
-    int get(int key) {
-        if(cache.find(key) == cache.end()) return -1;
-        
-        Lcache.splice(Lcache.begin(), Lcache, cache[key]); // update list
-        return cache[key]->second;
-    }
-    
-    void put(int key, int value) {        
-        if (get(key) != -1) { // if exists, replace value THATS IT (get function updates list)
-            cache[key]->second = value;
-            return;
-        }
-
-        if (cache.size() == cap) { // if !exists and cap, delete from map -> delete from list
-            int delKey = Lcache.back().first;
-            Lcache.pop_back();
-            cache.erase(delKey);
-        }
-        // if !exists, 
-        Lcache.emplace_front(key, value);
-        cache[key] = Lcache.begin();
-    }
-};
-
-// LIST.emplace_front => put item in front of list
-// LIST.splice(where to put position, list2, key to find); => move item to position in list
-// LIST.pop_back(); => remove last item in list
-// MAP.erase(); => erase item in map
 
 /**
  * Your LRUCache object will be instantiated and called as such:
