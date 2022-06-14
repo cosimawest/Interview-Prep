b78f265262674c45f59dd7630dc23c9cbe0fd134
cosimawest <44038882+cosimawest@users.noreply.github.com>
2022-06-14 12:20:28 -0400

    Update Multiply Strings.cpp

--- Google/Multiply Strings.cpp
+++ Google/Multiply Strings.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {   
-        if (num1 == "0" || num2 == "0") return "0";
+        if(num1 == "0" || num2 == "0") return "0";
         
         vector<int> res(num1.size()+num2.size(), 0);
+        string sum;
         
-        for (int i = num1.size()-1; i >= 0; i--) {
-            for (int j = num2.size()-1; j >= 0; j--) {
-                res[i + j + 1] += (num1[i]-'0') * (num2[j]-'0');
-                res[i + j] += res[i + j + 1] / 10;
-                res[i + j + 1] %= 10;
+        // num2 with the j pointer is acting like the top number in a hand written multiplation problem
+        // 
+        //  ###  <--num2
+        //   ##  <--num1
+        // x___
+        
+        for(int i = num1.size() - 1; i >= 0; i--) {
+            for(int j = num2.size() - 1; j >= 0; j--) {
+                res[i+j+1] += (num1[i]-'0') * (num2[j]-'0');
+                res[i+j] += res[i+j+1]/10;
+                res[i+j+1] %= 10;
             }
         }
         
         int i = 0;
-        string ans = "";
-        while (res[i] == 0) i++;
-        while (i < res.size()) ans += to_string(res[i++]);
+        while(res[i] == 0) i++;
+        while(i < res.size()) sum+=to_string(res[i++]);
         
-        return ans;
+        return sum;
     }
 };
