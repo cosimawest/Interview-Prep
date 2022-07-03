63cb8c5bf97a8246e85542d10d7af6bb9ae3b75e
cosimawest <44038882+cosimawest@users.noreply.github.com>
2022-07-03 14:15:11 -0400

    Update Binary Tree Maximum Path Sum.cpp

--- Google/Binary Tree Maximum Path Sum.cpp
+++ Google/Binary Tree Maximum Path Sum.cpp
@@ -37,3 +37,36 @@ public:
         return max_sum;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // check github for this code with explanation
+    int maxSum = INT_MIN;
+    
+    int maxPathSum(TreeNode* root) {
+        maxSum = max(maxSum, maxGain(root));
+        return maxSum;
+    }
+    
+    int maxGain(TreeNode* root) {
+        if(!root) return 0;
+        
+        int l = max(maxGain(root->left), 0);
+        int r = max(maxGain(root->right), 0);
+        
+        maxSum = max(maxSum, root->val + l + r);
+        
+        return max(root->val + l, root->val + r);
+    }
+};
