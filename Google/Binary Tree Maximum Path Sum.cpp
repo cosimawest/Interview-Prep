83579bdaf2fce38f4178ea853d1d8806ccdda291
cosimawest <44038882+cosimawest@users.noreply.github.com>
2022-07-03 14:26:36 -0400

    Update Binary Tree Maximum Path Sum.cpp

--- Google/Binary Tree Maximum Path Sum.cpp
+++ Google/Binary Tree Maximum Path Sum.cpp
@@ -11,62 +11,29 @@
  */
 class Solution {
 public:
-    int max_sum=INT_MIN;
-    
-    int max_gain(TreeNode* root) {
-        if(!root)return 0;
-        
-        // get value of left and right node, disregarding zeros
-        // we disregard zeros because along with wanting to use the max between left and right,
-        // we also want to check this current set of three nodes to see if it beats the current max
-        int l=max(max_gain(root->left),0);
-        int r=max(max_gain(root->right),0);
-        
-        // checking if current three nodes beat current max
-        int new_price=root->val+l+r;
-        max_sum=max(max_sum,new_price);
-        
-        // return to the parent node, it's current value plus the max between left and right side
-        // we only want one side, since after checking the partnet plus it's two children,
-        // we need to move upwards, meaning only one side of the branch can stay
-        return root->val+max(l,r);
-    }
-    
+    int res = INT_MIN;
     int maxPathSum(TreeNode* root) {
-        max_gain(root);
-        return max_sum;
-    }
-};
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    // check github for this code with explanation
-    int maxSum = INT_MIN;
-    
-    int maxPathSum(TreeNode* root) {
-        maxSum = max(maxSum, maxGain(root));
-        return maxSum;
+        // get longest path to the right and left
+        // add both (as long as one isn't negative) plus current and compare to max
+        // replace curr val with that number
+        getMaxPathSum(root);
+        
+        return res;
     }
     
-    int maxGain(TreeNode* root) {
+    int getMaxPathSum(TreeNode* root) {
         if(!root) return 0;
+        // return 0 if null
         
-        int l = max(maxGain(root->left), 0);
-        int r = max(maxGain(root->right), 0);
+        // left int equals maxPathSum left or 0
+        // right int equals maxPathSum right or 0
+        int l = max(getMaxPathSum(root->left), 0);
+        int r = max(getMaxPathSum(root->right), 0);
         
-        maxSum = max(maxSum, root->val + l + r);
+        // add left right and curr and compare to res
+        res = max(res, l + r + root->val);
         
-        return max(root->val + l, root->val + r);
+        // return max of left and right plus itself (curr will either increase or stay the same)
+        return max(l,r) + root->val;
     }
 };
