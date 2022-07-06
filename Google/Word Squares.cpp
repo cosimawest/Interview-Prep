8bf25c75e908fc659d81ed5a77eecbb36fea6853
cosimawest <44038882+cosimawest@users.noreply.github.com>
2022-07-06 17:38:40 -0400

    Update Word Squares.cpp

--- Google/Word Squares.cpp
+++ Google/Word Squares.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
+    // global variable so that we can manipulate the vector wo adding another parameter to the recursive function
     vector<vector<string>> res;
     
     vector<vector<string>> wordSquares(vector<string>& words) {
-        // the issue is that there isn't necesarilly just one answer per word
-        // organize words in a set for easier recall
-        // or a vector of vectors where the index is the first char of the word
-        vector<string> e = {};
         unordered_map<string, vector<string>> map;
         
-        // loop through each word in words and call a new function that with the word and index (0)
-        // this function returns a vector that we push back
-        // we can't just return a vector bc there could be multiple answers
-        
+        // we need to make a hashmap of vectors w the key being their prefix
+        // w, wa, wal, wall
+        // a, ar, are, area
+        // l, le, lea, lead
+        // l, la, lad, lady
+        // words sharing the same prefix will be in the vector together (ex: l => {lead, lady})
         for(auto word : words) {
             string temp = "";
             for(int i = 0; i < word.size(); i++) {
@@ -21,48 +20,39 @@ public:
             }
         }
         
+        vector<string> e = {};
         for(auto word : words)
             findWordSquare(word, word, 0, map, e);
         
-        return res;
-        
-        // create vector with first word
-        // if the index is the same as the size of the word push_back
-        // loop through the vector of vectors for the next letter, calling the recursive function with an 
-        // augmented index
-        // this will ensure that we don't just stop after adding one element
-        // on success, push_back
-        // on failure/exhastion return
-        // but don't return the recursive call, just call
-        // initial call will complete on exhaustion 
-        
-        // we need to make a hashmap of vectors the the key being their prefix
-        // w, wa, wal, wall
-        // a, ar, are, area
-        // l, le, lea, lead
-        // l, la, lad, lady
-        
-
+        return res;      
     }
     
     void findWordSquare(string word, string c, int i, unordered_map<string, vector<string>>& map, vector<string> t) {
-        //cout << "adding " << c << " and looking for " << word[i+1] << " for " << word << endl;
-        //if(c!=word) visited.insert(c);
+        // function will only be called on valid inputs so we can push right away
         t.push_back(c);
         
+        // sucess case: i will only get to this requirement
+        // if this function was called enough times
+        // could have also gone with t.size(), but that takes more time than comparing i's value
         if(i >= word.size()-1) {
             res.push_back(t);
             return;
         }
         
+        // we're checking if a word would make a valid word square as we go so reduce runtime
+        // to do that, we need to calculate the prefix that the next word needs to have to create a valid
+        // word square. This being, if this is the 3rd word added to the square, the 3rd char of each 
+        // previous word needs to be the prefix of this next word
         string prefix = "";
         if(i+1 < word.size()) 
-            for(auto s : t) prefix.push_back(s[i+1]);
+            for(auto s : t) 
+                prefix.push_back(s[i+1]);
         
-        if(map[prefix].empty()) return;
+        // we check if there are any words with this prefix, and return if not
+        if(map[prefix].empty()) 
+            return;
 
-        // take the i th element of every word in t and 
-        // the word that starts with those chars is what you're looking for
+        // if there are words with this prefix, run through all of them
         for(auto e : map[prefix]) 
             findWordSquare(word, e, i+1, map, t);
         
