0c5cc496725f5e03bdcb1209e7e2ee37c9806981
cosimawest <44038882+cosimawest@users.noreply.github.com>
2022-07-10 11:07:43 -0400

    Create Median of Two Sorted Arrays.cpp

---
+++ Google/Median of Two Sorted Arrays.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
+        
+        int low = 0;
+        int high = nums1.size();
+        
+        int maxLeftX, maxLeftY, minRightX, minRightY;
+        
+        while(low <= high) {
+            int positionX = (low + high) / 2;
+            int positionY = (nums1.size() + nums2.size() + 1) / 2 - positionX;
+            
+            maxLeftX = (positionX == 0) ? INT_MIN : nums1[positionX - 1];
+            minRightX = (positionX == nums1.size()) ? INT_MAX : nums1[positionX];
+            
+            maxLeftY = (positionY == 0) ? INT_MIN : nums2[positionY - 1];
+            minRightY = (positionY == nums2.size()) ? INT_MAX : nums2[positionY];
+            
+            if(maxLeftX > minRightY) high = positionX-1;
+            else if(minRightX < maxLeftY) low = positionX+1;
+            else break;
+        }
+        
+        if((nums1.size() + nums2.size()) % 2 == 0) //even
+            return (double)(max(maxLeftX, maxLeftY) + min(minRightX, minRightY) / 2);
+        else
+            return (double)max(maxLeftX, maxLeftY);
+        
+    }
+};
+
+
+/*
+
+low = 0;
+high = x.size();
+
+while(low<high)
+
+positionX = low + high / 2
+positionY = (x.size + y.size + 1 / 2) - positionX;
+
+check if positionX is empty on the left or right
+
+left? maxLeftX = INT_MIN
+right? minRightX = INT_MAX
+
+maxLeftX
+minRightX
+
+maxLeftY
+minRightY
+
+if(maxLeftX <= minRightY && minRightX >= maxLeftY) yay!!
+else if(maxLeftX > minRightY) positionX--
+else positionX++
+
+
+
+*/
