7c407d7ae729f514f63f6b85119d53e81015ae38
cosimawest <44038882+cosimawest@users.noreply.github.com>
2022-07-13 12:01:18 -0400

    Update Longest Palindromic Substring.cpp

--- Google/Longest Palindromic Substring.cpp
+++ Google/Longest Palindromic Substring.cpp
@@ -28,3 +28,51 @@ public:
 };
 
 // dp solution
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        // dp version
+        // create bool dp of s.size() x s.size() size
+        // make dp[i][i] = t
+        // dp[i][i+1] = t if s[i] = s[i+1]
+        // then fill the rest of the dp based on if the diagonal line before it is true, and if the ends are
+        // the same char
+        
+        /* dp[0][1] check s[0] == s[1]
+           dp[1][4] check s[1] == s[4], dp[2][3] == 1 , d[i++][j--] while i <= j
+          a b e e b h
+        a 1 0 0 0
+        b   1 0 0 1
+        e     1 1 0     
+        e       1 0 0
+        b         1 0
+        h           1
+        */
+        int n = s.size();
+        string res = "";
+        vector<vector<bool>> dp(n, vector<bool>(n, 0));
+        
+        // build first two lines of dp
+        for(int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+            if(i < n-1 && s[i]==s[i+1]) dp[i][i+1] = 1;
+        }
+        
+        for(int i = 2; i < n; i++) {
+            for(int j = 0; i+j < n; j++) {
+                int r = j, c = i+j;
+                dp[j][i+j] = s[r]==s[c] && dp[r+1][c-1];
+            }
+        }
+        
+        pair<int, int> maxS = {0,0};
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(dp[i][j] && j-i > maxS.second-maxS.first) maxS.first = i, maxS.second = j;
+            }
+        }
+                
+        return s.substr(maxS.first, maxS.second-maxS.first+1);
+    }
+};
